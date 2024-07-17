6d94f11af9a618596c2745909fdd1edc1e3edc87
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2024-07-17 05:58:11 -0400

    Working with spatial data - a couple of simple examples

---
+++ spei.R
@@ -0,0 +1,45 @@
+# SPEI
+
+library(tibble)
+coords <- tribble(
+  ~place,  ~lon,         ~lat,
+  "JGCRI",    -76.92238, 38.97160,
+  "Thompson", -97.84862, 55.74706,
+  "Fes",      -5.01007,  34.03614,
+  "Lima",     -77.03086, -12.04545
+)
+coords$ID <- seq_len(nrow(coords))
+
+
+# Documentation: https://spei.csic.es/database.html
+# Data downloaded 2024-07-17
+
+# These data are provided as netCDF files (http://en.wikipedia.org/wiki/NetCDF)
+# Happily, the terra package can handle these!
+# (There's also the `ncdf4` package for many other applications)
+library(terra)
+spei <- rast("spei_data/spei01.nc")
+# Confirm that things look good -- it's a spatial raster object,
+# global half degree resolution, from January 1901 to December 2022
+print(spei)
+
+# Extract our points of interest. terra::extract() will handle making sure
+# the coordinates get mapped to the correct grid cell(s) in the data
+spei_coords <- terra::extract(spei, coords[2:3])
+# 1465 columns! Why? Because that's 122 years * 12 months + "ID" column
+
+# Reshape data into a more manageable form
+library(tidyr)
+spei_monthly <- pivot_longer(spei_coords, -ID)
+spei_monthly <- separate(spei_monthly, name, into = c("spei", "entry"), convert = TRUE)
+# The SPEI data don't seem to provide 'time' explicitly in the netcdf, so
+# compute it from the entries
+spei_monthly$year <- ceiling(spei_monthly$entry / 12) + 1900
+spei_monthly$month <- (spei_monthly$entry - 1) %% 12 + 1
+spei_monthly$time <- with(spei_monthly, year + (month-1) / 12)
+spei_monthly <- dplyr::left_join(spei_monthly, coords, by = "ID")
+
+# Not sure about the best way to plot this
+p <- ggplot(spei_monthly, aes(time, value, color = place)) +
+  geom_point(size = 0.25) + geom_smooth()
+print(p)
---
+++ worldclim.R
@@ -0,0 +1,51 @@
+# WorldClim data
+
+library(tibble)
+coords <- tribble(
+  ~place,  ~lon,         ~lat,
+  "JGCRI",    -76.92238, 38.97160,
+  "Thompson", -97.84862, 55.74706,
+  "Fes",      -5.01007,  34.03614,
+  "Lima",     -77.03086, -12.04545
+)
+coords$ID <- seq_len(nrow(coords))
+
+# These data are available online but R's geodata package will download
+# for us -- handy!
+
+library(geodata)
+# geodata::worldclim_global is smart enough to *cache* the data--
+# it will only download if the data don't exist at the path we give it
+tavg <- worldclim_global("tavg", "10", "worldclim_data/")
+# Note that we've told Git to IGNORE these data files; see ".gitignore" file
+
+# The resulting object is a spatial raster object. See ?SpatRaster
+dim(tavg)
+print(tavg)
+plot(tavg)
+
+# Extract our points of interest. terra::extract() will handle making sure
+# the coordinates get mapped to the correct grid cell(s) in the data
+library(terra)
+tavg_coords <- terra::extract(tavg, coords[2:3])
+
+# The result is a data frame with one row per coordinate and
+# one column per month
+class(tavg_coords)
+dim(tavg_coords)
+# If I want the monthly data, I would probably *reshape* these data
+# In this case, let's compute MAT (mean annual temperature)
+coords$MAT <- rowMeans(tavg_coords[-1]) # Why "-1"?
+
+# Look at seasonal cycle
+library(tidyr)
+monthly <- pivot_longer(tavg_coords, -ID)
+# Extract month number from 'name'
+monthly$month <- as.numeric(gsub("wc2.1_10m_tavg_", "", monthly$name))
+# ...and join with coords to get the name of each place
+monthly <- dplyr::left_join(monthly, coords)
+
+library(ggplot2)
+p <- ggplot(monthly, aes(month, value, color = place)) + geom_line() + ylab("Air temperature")
+print(p)
+
